ff31114389ad6fb231e62fc38140aafc561f66b1
TurkiHaqawi <Turki.Haqawii@gmail.com>
2022-02-15 15:17:46 +0300

    fix comments

--- lab.py
+++ lab.py
@@ -1,20 +1,20 @@
 # 1)
-# numbers = range(45, 210)
-# for num in numbers:
-#     if num == 100:
-#         continue
-#     elif num == 206:
-#         break
-#     print(num)
+numbers = range(45, 210)
+for num in numbers:
+    if num == 100:
+        continue
+    elif num == 206:
+        break
+    print(num)
 
 
 # 2)
-# state = True
-# while state:
-#     result = input("what is the product of 7 * 24?")
-#     if int(result) == 168:
-#         print("You answered this Question correctly")
-#         state = False
-#     else:
-#         print("Your Answer is wrong try again..")
-#         state = True
+state = True
+while state:
+    result = input("what is the product of 7 * 24?")
+    if int(result) == 168:
+        print("You answered this Question correctly")
+        state = False
+    else:
+        print("Your Answer is wrong try again..")
+        state = True
