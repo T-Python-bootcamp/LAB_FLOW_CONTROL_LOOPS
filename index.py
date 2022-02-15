2dad5a54fd66ec16d3f0b05f1a8d31b9c2c87484
walaTuwaiq <wala.radi.rae.js@tuwaiq.edu.sa>
2022-02-15 12:15:51 +0300

    result

---
+++ index.py
@@ -0,0 +1,15 @@
+# make a range from 45 to 210, using a for loop iterate over the sequence and print the elements. Skip the number 100 and break the loop at 205
+# for val in range(45,210):
+#     if val == 100:
+#         continue
+#     if val == 205:
+#         break
+#     print(val)
+
+resultUser = input("what is the product of 7 * 24 ?")
+if resultUser == "168":
+    print("You answered this Question correctly")
+else:
+    print("Your Answer is wrong try again..")
+    resultUser = input("what is the product of 7 * 24 ?")
+
