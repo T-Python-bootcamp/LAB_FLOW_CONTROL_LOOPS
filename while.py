784d7b3b8a490a41ff580bfec0145e07a98aec05
Basel Alanazi <alanaziibasel@gmail.com>
2022-02-15 12:26:41 +0300

    Create files

---
+++ while.py
@@ -0,0 +1,10 @@
+num = 7 * 24
+    
+while True:
+    value = input("what is the product of 7 * 24 ?")
+    if int(value) == num:
+        print ("You answered this Question correctly")
+        break
+    else:
+        print("Your Answer is wrong try again:")
+
