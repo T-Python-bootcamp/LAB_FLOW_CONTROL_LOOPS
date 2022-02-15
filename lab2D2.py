4bf9b46ce09d9c071ec9b8c6516e14e77852f853
Alanoudalmakhalafi <alanoudm540@gmail.com>
2022-02-15 14:26:23 +0300

    done

---
+++ lab2D2.py
@@ -0,0 +1,19 @@
+
+# for number in range(45, 210):
+#     if number == 100:
+#         continue
+#     elif number == 206:
+#         break
+#     print(number)
+
+
+number = 7 * 24
+    
+while True:
+    value = input("what is the product of 7 * 24 ?")
+    if int(value) == number:
+        print ("You answered this Question correctly")
+        break
+    else:
+        print("Your Answer is wrong try again..")
+        
