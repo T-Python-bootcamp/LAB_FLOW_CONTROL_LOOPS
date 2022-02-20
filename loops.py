78b7400d3896d15c6091e091fb71313db7f0c2a2
RawanZaben <rawan.zaben.rae.js@tuwaiq.edu.sa>
2022-02-20 18:38:28 +0300

    lab loops

---
+++ loops.py
@@ -0,0 +1,18 @@
+for num in range(45 , 210):
+    if num == 100:
+        continue
+
+    elif num == 205:
+        break
+    print(num)
+    
+    ###############################
+
+ret=168
+while True:
+    ask = input('What is the product of 7 * 24 ? ')
+    if ask == str(ret) :
+        break
+        print('Your Answer is wrong try again..')
+
+print('You answered this Question correctly') 
