13ef6f45cbd51d04d9388ff16c7c6cdef0d60e20
KhaledHaAlmsoud <Khaled.Almsoud.rae.js@tuwaiq.edu.sa>
2022-02-20 18:13:02 +0300

    task

---
+++ task.py
@@ -0,0 +1,26 @@
+# for num in range(45,210):
+
+#     if num == 100:
+#         continue
+#     elif num == 205:
+#         break
+
+#     print(num)
+
+
+
+
+
+ask = input ("what is the product of 7 * 24 ?")
+print(ask)
+
+converToint = int (ask)
+while converToint == 168 :
+ 
+    print ("You answered this Question correctly")
+    break
+else :
+    print ("Your Answer is wrong try again..")
+    ask = input ("what is the product of 7 * 24 ?")
+
+
